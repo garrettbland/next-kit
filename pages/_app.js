fa5c4831cbe2f83478e224083f9ca60514683959
garrettbland <gmorganbland@gmail.com>
2021-04-19 17:15:11 -0500

    initial setup & commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import '../styles/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,88 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import siteconfig from '../siteconfig.json'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+const Home = () => {
+    return (
+        <>
+            <Head>
+                <title>
+                    {siteconfig.title} by {siteconfig.author}
+                </title>
+            </Head>
+            <div className="max-w-2xl mx-auto p-8 prose prose">
+                <small>
+                    Created by{' '}
+                    <Link prefetch={false} href="https://github.com/garrettbland">
+                        <a>https://github.com/garrettbland</a>
+                    </Link>
+                </small>
+                <h1>{siteconfig.title}</h1>
+                <p>
+                    Next Kit is a{' '}
+                    <Link href="#" prefetch={false}>
+                        Next.js
+                    </Link>{' '}
+                    website starter kit with some popular technologies and an opinionated setup. Its
+                    purpose is to give you a head start for your website projects so you can dive
+                    into development quicker.
+                </p>
+                <Link prefetch={false} href="#">
+                    <a
+                        className="inline-block bg-gray-900 hover:bg-gray-700 rounded-xl py-3 px-6 text-lg transition-all focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                        style={{ color: 'white', textDecoration: 'none' }}
+                    >
+                        Use this template
+                    </a>
+                </Link>
+                <div className="h-px w-full bg-blueGray-200 my-8"></div>
+                <h2>Purpose</h2>
+                <p>
+                    Any new site setup takes a bit of work to get fully up and going. This is a
+                    template that lets you fire up a Next.js site up quickly with all the goodies
+                    setup out of the box.
+                </p>
+                <h2>Features</h2>
+                <ul>
+                    <li>Next.js</li>
+                    <li>Tailwind CSS</li>
+                    <li>PostCSS & Autoprefixer</li>
+                    <li>Typescript</li>
+                    <li>Typography plugin for Tailwind</li>
+                    <li>Prettier</li>
+                </ul>
+                <h2>Getting Started</h2>
+                <p>
+                    Getting started is easy, just{' '}
+                    <Link href="#" prefetch={false}>
+                        <a>click here</a>
+                    </Link>{' '}
+                    to use this template. You will be taken to github to create a new repo using
+                    this template. If you would rather look at the code and use the setup in your
+                    existing environment,{' '}
+                    <Link href="#" prefetch={false}>
+                        <a>click here</a>
+                    </Link>{' '}
+                    to look at the public repo.
+                </p>
+                <div className="h-px w-full bg-blueGray-200 my-8"></div>
+                <h3>Bonus Stuff</h3>
+                <p>
+                    Next.js comes with optional server side routing and API routes. These can be
+                    utilized on places like{' '}
+                    <Link href="#" prefetch={false}>
+                        <a>Netlify</a>
+                    </Link>
+                    (like this site) or{' '}
+                    <Link href="#" prefetch={false}>
+                        <a>Vercel</a>
+                    </Link>{' '}
+                    to name a few. Netlify for example will now detect when you are using Next.js,
+                    and will automatically setup serverless functions for your SSR routes.
+                </p>
+            </div>
+        </>
+    )
 }
+
+export default Home
